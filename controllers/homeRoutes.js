0a0a5d98108e6a4738cacbc8aa6209e36242b286
unknown <hannahnmcdonald@protonmail.com>
2021-10-06 12:23:44 -0400

    troubleshooting logged in sessions

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { Review, User, Club } = require("../models");
-const withAuth = require("../utils/auth");
+// const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
@@ -50,29 +50,29 @@ router.get("/", async (req, res) => {
 // });
 
 // Use withAuth middleware to prevent access to route
-router.get("/profile", withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"] },
-      include: [{ model: Review }],
-    });
+// router.get("/profile", withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ["password"] },
+//       include: [{ model: Review }],
+//     });
 
-    const user = userData.get({ plain: true });
+//     const user = userData.get({ plain: true });
 
-    res.render("profile", {
-      ...user,
-      logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render("profile", {
+//       ...user,
+//       logged_in: true,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect("/profile");
+    res.redirect("/");
     return;
   }
   res.render("login");
@@ -80,7 +80,10 @@ router.get("/login", (req, res) => {
 
 //This route allows the nav button "write a review" to go to the search book page
 router.get("/searchbook", (req, res) => {
-  res.render("searchbook");
+  res.render("searchbook", {
+    loggedIn: req.session.loggedIn
+  });
+  
 });
 
 //This route allows the nav button "Register" to go to the Register page
@@ -153,8 +156,8 @@ router.get('/clubs', async (req, res) => {
   const clubs = clubData.map((club) => club.get({ plain:true}));
   console.log(clubs)
   res.render('clubs', {
-    clubs
-    // logged_in: req.session.logged_in
+    clubs,
+    logged_in: req.session.logged_in
   });
 } catch (err) {
   console.log(err)
